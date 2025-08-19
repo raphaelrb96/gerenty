f25a5366154a0b42c098d9adce104d1f073132ea
Raphael Almeida <raphael.rb96@gmail.com>
2025-08-19 17:54:36 +0000

    REFAÇA O LAYOUT PARA CORRIJIR

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,11 +9,11 @@ export default function DashboardLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen">
+      <div className="flex min-h-screen bg-muted/40">
         <MainSidebar />
         <SidebarInset>
           <Header />
-          <main className="flex-1 w-full">
+          <main className="flex-1">
             <div className="p-6 md:p-8">
               {children}
             </div>
