1ac9415f0a215cb1cc513d46735e2ca9aa54a6fe
Raphael Almeida <raphael.rb96@gmail.com>
2025-08-28 18:25:17 +0000

    O projeto ja esta hospedado no firebase hosting mais as imagens nao esta

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
