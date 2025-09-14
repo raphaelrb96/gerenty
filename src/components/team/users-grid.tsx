692dec917d84163109fc5bbedece19d9602e8680
Raphael Almeida <raphael.rb96@gmail.com>
2025-09-14 04:34:23 +0000

    coloque duas colunas em dispositivos maiores e uma coluna em dispositivo

--- src/components/team/users-grid.tsx
+++ src/components/team/users-grid.tsx
@@ -60,7 +60,7 @@ export function UsersGrid({ data, onEdit, onDelete, onManageAccess }: UsersGridP
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {data.map((member) => (
               <Card key={member.id}>
                 <CardHeader className="flex flex-row items-center justify-between">
