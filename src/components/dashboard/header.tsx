480fef5ac02c3cc903c4d734ae775027e6a137ad
Raphael Almeida <raphael.rb96@gmail.com>
2025-09-17 02:24:46 +0000

    o headbar precisa ser responsivo entao nao pode ter largura minima e nem

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -37,7 +37,7 @@ export function Header() {
   }
 
   return (
-    <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:px-6">
+    <header className="sticky top-0 z-10 flex h-14 w-full items-center gap-4 border-b bg-background px-4 sm:px-6">
       <SidebarTrigger className="md:hidden" />
 
       <div className="flex-1">
